a85a8f2f02837f260c581a4e60819c09a3425939
Md. Ishtiak Ahmed <42693745+ishtiakrongon@users.noreply.github.com>
2022-07-04 23:04:26 +0600

    Set Theory clauses

--- joining_tables.sql
+++ joining_tables.sql
@@ -284,4 +284,40 @@ LIMIT 15;
 --------------------------------------------------------------------------------
 
 
+-- UNION
+-- Combine the two new tables into one table containing all of the 
+-- fields in economies2010
+
+SELECT *
+FROM economies2010
+UNION ALL
+SELECT *
+FROM economies2015
+ORDER BY code, year;
+
+
+--------------------------------------------------------------------------------
+
+
+-- Determine all (non-duplicated) country codes in either the cities or the 
+-- currencies table. The result should be a table with only one field called country_code.
+-- Sort by country_code in alphabetical order
+
+SELECT
+  country_code
+FROM
+  cities
+UNION
+SELECT
+  code
+FROM
+  currencies
+ORDER BY country_code;
+
+
+--------------------------------------------------------------------------------
+
+
+
+
 
