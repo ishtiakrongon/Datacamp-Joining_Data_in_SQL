f6840a44242a1b1a92f42d2775b47ab5705ec472
Md. Ishtiak Ahmed <42693745+ishtiakrongon@users.noreply.github.com>
2022-07-05 01:25:23 +0600

    Update joining_tables.sql

--- joining_tables.sql
+++ joining_tables.sql
@@ -352,6 +352,25 @@ ORDER BY code, year;
 
 --------------------------------------------------------------------------------
 
+-- EXCEPT
+
+-- Select field
+SELECT name
+  -- From cities
+  FROM cities
+	-- Set theory clause
+	EXCEPT
+-- Select field
+SELECT capital
+  -- From countries
+  FROM countries
+-- Order by result
+ORDER BY name;
+
+
+--------------------------------------------------------------------------------
+
+
 
 
 
